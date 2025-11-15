bfe3d0b382aaab543985c33c106836e902f19dd2
Radek Vala <radek.www@gmail.com>
2025-11-15 10:53:11 +0100

    V aktivitě instnciuj Repository + VM a propoj VM s UI přes binding

--- app/src/main/java/cz/utb/fai/subjectinfo/SubjectInfoActivity.kt
+++ app/src/main/java/cz/utb/fai/subjectinfo/SubjectInfoActivity.kt
@@ -6,12 +6,20 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.viewmodel.viewModelFactory
 import cz.utb.fai.subjectinfo.databinding.ActivitySubjectinfoBinding
 
 class SubjectInfoActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySubjectinfoBinding
-    // 2. Initialize the ViewModel using the by viewModels() delegate
-    private val viewModel: SubjectInfoViewModel by viewModels()
+
+    // 1. Get the repository instance from the Application class
+    private val repository by lazy { (application as MyApplication).repository }
+
+    // 2. Create the factory using the repository from the Application class
+    private val viewModelFactory by lazy { SubjectInfoViewModelFactory(repository) }
+
+    // 3. Pass the factory to the viewModels() delegate
+    private val viewModel: SubjectInfoViewModel by viewModels { viewModelFactory }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +28,9 @@ class SubjectInfoActivity : AppCompatActivity() {
         binding = ActivitySubjectinfoBinding.inflate(layoutInflater)
         // nastavení layoutu
         setContentView(binding.root)
+        // propojení VM s XML UI
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
 
         ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
