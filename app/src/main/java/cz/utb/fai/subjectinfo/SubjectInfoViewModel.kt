f1c4eba7c1c832e6a739d7847409f97bc6a4788d
Radek Vala <radek.www@gmail.com>
2025-11-15 10:05:31 +0100

    pridej vlastnosti a metody view modelu

--- app/src/main/java/cz/utb/fai/subjectinfo/SubjectInfoViewModel.kt
+++ app/src/main/java/cz/utb/fai/subjectinfo/SubjectInfoViewModel.kt
@@ -1,7 +1,53 @@
 package cz.utb.fai.subjectinfo
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import cz.utb.fai.subjectinfo.model.SubjectInfo
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
 
 class SubjectInfoViewModel : ViewModel() {
 
+    private val _subjectInfoValue = MutableLiveData<SubjectInfo>()
+    val subjectInfoValue: LiveData<SubjectInfo> = _subjectInfoValue
+
+    val showHint = MutableLiveData<Boolean>()
+    val showNotFound = MutableLiveData<Boolean>()
+
+    val zkratkaMutable = MutableLiveData<String?>()
+
+
+
+    fun getSubjectInfo(katedra: String, zkratka: String){
+        /*
+        viewModelScope.launch {
+            val result = repository.getSubjectInfo(katedra, zkratka)
+            if(result is SubjectInfo) {
+                _subjectInfoValue.value = result
+                showNotFound.value = false
+            } else {
+                showNotFound.value = true
+            }
+        }
+
+         */
+    }
+
+    fun search () {
+        if (zkratkaMutable.value != null && !zkratkaMutable.value!!.isEmpty()) {
+            // zkratka was provided by the user
+            getSubjectInfo("AUIUI", zkratkaMutable.value!!)
+        } else {
+            // zkratka was not provided, show hint text view
+            showHint.value = true
+        }
+    }
+
+    fun hideHintAndNotFound () {
+        showHint.value = false
+        showNotFound.value = false
+    }
 }
