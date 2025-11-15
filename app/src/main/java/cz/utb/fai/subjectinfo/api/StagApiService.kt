0e1e0c9a2855060503c0a2b48332345f85814ba5
Radek Vala <radek.www@gmail.com>
2025-11-15 10:14:08 +0100

    pridej retrofit interface pro definici jednoho HTTP GET volani

---
+++ app/src/main/java/cz/utb/fai/subjectinfo/api/StagApiService.kt
@@ -0,0 +1,24 @@
+package cz.utb.fai.subjectinfo.api
+
+import cz.utb.fai.subjectinfo.model.SubjectInfo
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface StagApiService {
+
+    companion object {
+        const val PATH = "predmety/getPredmetInfo"
+    }
+    /**
+     * Fetches a list of subjects for a given department.
+     * This is a suspend function to be used with coroutines.
+     * The URL called will be: BASE_URL + "subject/getSubjectsForDepartment"
+     * Example with query: BASE_URL + "subject/getSubjectsForDepartment?department=UAI"
+     */
+    @GET(PATH) // 4. Define the endpoint path
+    suspend fun getSubjectInfo(
+        @Query("katedra") katedra: String, // 5. Define a query parameter
+        @Query("zkratka") zkratka: String, // 5. Define a query parameter
+        @Query("outputFormat") outputFormat: String = "json" // 5. Define a query parameter
+    ): SubjectInfo? // 6. Define the return type
+}
