2d996a9bb0b45e9c34c8e5d1c979e9c82d7d66c5
Radek Vala <radek.www@gmail.com>
2025-12-13 12:02:04 +0100

    compose compiler added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,12 @@
+
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     // Room DB needs KSP
     alias(libs.plugins.ksp)
+
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
@@ -39,6 +43,10 @@ android {
     buildFeatures {
         viewBinding = true
         dataBinding = true
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.15" // Ensure this matches your Kotlin version
     }
 }
 
@@ -64,6 +72,24 @@ dependencies {
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
     ksp(libs.androidx.room.compiler)
+
+    // Compose
+    // Compose Dependencies
+    val composeBom = platform(libs.androidx.compose.bom)
+    implementation(composeBom)
+    androidTestImplementation(composeBom)
+
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    // Crucial for your existing ViewModel migration
+    implementation(libs.androidx.compose.runtime.livedata)
+
+    debugImplementation(libs.androidx.compose.ui.tooling)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
